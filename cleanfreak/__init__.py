e427d2c199bc9dfa35f2874eff753012abb35050
Dan Bradham <danielbradham@gmail.com>
2015-02-02 11:31:31 -0500

    0.1.6 updated default config mayacheckers

--- cleanfreak/__init__.py
+++ cleanfreak/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'cleanfreak'
 __author__ = 'Dan Bradham'
 __email__ = 'danielbradham@gmail.com'
 __url__ = 'http://github.com/danbradham/cleanfreak'
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 __license__ = 'MIT'
 __description__ = 'Sanity checks and grades for CG production.'
 
